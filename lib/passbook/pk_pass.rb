2a88e7e44b622294ff3ec21dcd5926fe23ba8f79
Cody Veal <cveal05@gmail.com>
2012-09-24 11:07:39 -0400

    AndreiD - adding supports for translation strings, refactoring

--- lib/passbook/pk_pass.rb
+++ lib/passbook/pk_pass.rb
@@ -6,27 +6,23 @@ require 'json'
 require 'openssl'
 require 'zip/zip'
 require 'zip/zipfilesystem'
-require 'open-uri'
 
+#TODO: warn about requirements for different pass types
 
 module Passbook
 
   class Pkpass
-    attr_accessor :json, :template_path, :pass_type_id, :wwdr_intermediate_certificate_path,:serial_number, :files, :certificate_url, :certificate_password, :temporary_directory, :temporary_path, :manifest_url, :signature_url
+    attr_accessor :translations, :json, :template_path, :pass_type_id, :wwdr_intermediate_certificate_path, :serial_number, :certificate_url, :certificate_password, :temporary_directory, :temporary_path, :manifest_url
 
     def initialize(pass_type_id, serial_number, template_path)
-      puts "$$$$$$$$$$$$$$$!!!!!!!!!!!!!!!!!!"
       self.pass_type_id = pass_type_id
       self.serial_number = serial_number
       self.template_path = template_path
-      create_temporary_directory
-      copy_template_to_temporary_directory
-      self.files = []
-    end
+      self.translations = Hash.new
 
-    def copy_template_to_temporary_directory
-      puts "copying from #{self.template_path} to #{self.temporary_directory}"
-      FileUtils.cp_r(self.template_path, self.temporary_directory)
+      self.create_temporary_directory
+      self.copy_template_to_temporary_directory
+      self.read_json
     end
 
     def add_file content, file_name
@@ -39,28 +35,59 @@ module Passbook
       File.open(file_path, "w") do |f|
         f.write content.force_encoding('UTF-8')
       end
-      self.files << file_path
+    end
+
+    def add_translation_string source, destination, language
+      self.translations[language] = Hash.new unless self.translations.include?(language)
+      self.translations[language][source] = destination
     end
 
     def package
-      #write out pass.json
-      File.open(self.temporary_path+"/pass.json", "w") do |f|
-        f.write JSON.pretty_generate(self.json)
-      end
-
-      #do all the stuff that sign_pass does right now
+      self.write_json
+      self.write_translation_strings
       self.clean_ds_store_files
       self.generate_json_manifest
       self.sign_manifest
       self.compress_pass_file
-
     end
 
+    def output_url
+      self.temporary_directory+"/"+self.serial_number+".pkpass"
+    end
+
+
+
+    def read_json
+      self.json = JSON.parse(File.read("#{self.temporary_path}/pass.json"))
+    end
+
+    def write_json
+      File.open(self.temporary_path+"/pass.json", "w") do |f|
+        f.write JSON.pretty_generate(self.json)
+      end
+    end
+
+    def copy_template_to_temporary_directory
+      puts "copying from #{self.template_path} to #{self.temporary_directory}"
+      FileUtils.cp_r(self.template_path, self.temporary_directory)
+    end
+
+    def write_translation_strings
+      self.translations.each do |language, trans|
+        translations_path = "#{self.temporary_path}/#{language}.lproj"
+        FileUtils.mkdir_p(translations_path) unless File.directory?(translations_path)
+        File.open("#{translations_path}/pass.strings", "a") do |f|
+          trans.each do |key, value|
+            f.puts "\"#{key}\" = \"#{value}\";"
+          end
+        end
+      end
+    end
 
     # Creates a temporary place to work with the pass files without polluting the original
     def create_temporary_directory
       self.temporary_directory = Dir.mktmpdir
-      puts "!!!Creating temp dir at #{self.temporary_directory}"
+      puts "Creating temp dir at #{self.temporary_directory}"
       self.temporary_path = self.temporary_directory + "/" + self.pass_type_id
       # Check if the directory exists
       if File.directory?(self.temporary_path)
@@ -70,15 +97,12 @@ module Passbook
       FileUtils.mkdir_p(self.temporary_path)
     end
 
-
     def clean_ds_store_files
       puts "Cleaning .DS_Store files"
       Dir.glob(self.temporary_path + "**/.DS_Store").each do |file|
         File.delete(file)
       end
-
     end
-
 
     # Creates a json manifest where each files contents has a SHA1 hash
     def generate_json_manifest
@@ -86,7 +110,6 @@ module Passbook
       manifest = {}
 
       # Gather all the files and generate a sha1 hash
-      #Matching pattern was "/**" and now changed to ".*"
       Dir.glob(self.temporary_path + "/**/**").each do |file|
         manifest[file[self.temporary_path.length+1..-1].gsub("/", "\/")] = Digest::SHA1.hexdigest(File.read(file)) unless File.directory?(file)
       end
@@ -96,7 +119,6 @@ module Passbook
       File.open(self.manifest_url, "w") do |f|
         f.write(JSON.pretty_generate(manifest))
       end
-
     end
 
     def sign_manifest
@@ -118,11 +140,6 @@ module Passbook
       end
     end
 
-
-    def output_url
-      self.temporary_directory+"/"+self.serial_number+".pkpass"
-    end
-
     def compress_pass_file
       puts "Compressing the pass"
       zipped_file = File.open(self.output_url, "w")
@@ -138,9 +155,5 @@ module Passbook
       end
       zipped_file
     end
-
-
   end
-
 end
-
