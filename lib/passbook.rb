643e314b63fc87e7757138f7d7091ccc22264883
Andrei D & Vincent L <andrei.dinin@xtremelabs.com>
2012-10-04 19:21:05 -0400

    AndreiD - updating pkpass to use memory instead of filesystem

--- lib/passbook.rb
+++ lib/passbook.rb
@@ -1,11 +1,34 @@
 require "passbook/pk_pass"
+require 'fileutils'
 
 module Passbook
-  mattr_accessor :pass_config, :wwdr_intermediate_certificate_path
+  mattr_accessor :pass_config, :wwdr_intermediate_certificate_path, :wwdr_certificate
 
   def self.configure
     yield self
+    read_template
+    read_certificates
   end
+
+  def self.read_template
+    pass_config.each do |pass_type_id, config|
+      config['files'] = Hash.new
+      Dir.glob(config['template_path']+"/**/**").each do |file|
+        next if File.directory? file
+        content = File.read(file)
+        filename = Pathname.new(file).relative_path_from(Pathname.new(config['template_path']))
+        config['files'][filename.to_s]=content
+      end
+    end
+  end
+
+  def self.read_certificates
+    pass_config.each do |pass_type_id, config|
+      config['p12_certificate'] =  OpenSSL::PKCS12::new(File.read(config['cert_path']), config['cert_password'])
+    end
+    self.wwdr_certificate = OpenSSL::X509::Certificate.new(File.read(wwdr_intermediate_certificate_path))
+  end
+
 end
 
 
--- lib/passbook/pk_pass.rb
+++ lib/passbook/pk_pass.rb
@@ -1,6 +1,4 @@
 require 'rubygems'
-require 'fileutils'
-require 'tmpdir'
 require 'digest/sha1'
 require 'json'
 require 'openssl'
@@ -12,7 +10,7 @@ require 'zip/zipfilesystem'
 module Passbook
 
   class Pkpass
-    attr_accessor :translations, :json, :pass_type_id, :serial_number, :temporary_directory, :temporary_path, :manifest_url, :config
+    attr_accessor :files, :translations, :json, :pass_type_id, :serial_number,  :config
 
     def initialize(pass_type_id, serial_number)
       self.pass_type_id = pass_type_id
@@ -20,21 +18,12 @@ module Passbook
       self.translations = Hash.new
       self.config = Passbook.pass_config[self.pass_type_id]
 
-      self.create_temporary_directory
-      self.copy_template_to_temporary_directory
-      self.read_json
+      self.files = self.config['files'].dup
+      self.json = JSON.parse(self.files['pass.json'])
     end
 
-    def add_file content, file_name
-      if file_name.include? "/"
-        folder_name = file_name.gsub(file_name.split("/").last, "")
-        folder_path = self.temporary_path+"/"+folder_name
-        FileUtils.mkdir_p(folder_path) unless File.directory?(folder_path)
-      end
-      file_path = self.temporary_path+"/"+file_name
-      File.open(file_path, "w") do |f|
-        f.write content.force_encoding('UTF-8')
-      end
+    def add_file filename, content
+      self.files[filename] = content
     end
 
     def add_translation_string source, destination, language
@@ -45,62 +34,21 @@ module Passbook
     def package
       self.write_json
       self.write_translation_strings
-      self.clean_ds_store_files
       self.generate_json_manifest
       self.sign_manifest
       self.compress_pass_file
     end
 
-    def output_url
-      self.temporary_directory+"/"+self.serial_number+".pkpass"
-    end
-
-
-
-    def read_json
-      self.json = JSON.parse(File.read("#{self.temporary_path}/pass.json"))
-    end
-
     def write_json
-      File.open(self.temporary_path+"/pass.json", "w") do |f|
-        f.write JSON.pretty_generate(self.json)
-      end
-    end
-
-    def copy_template_to_temporary_directory
-      puts "copying from #{self.config['template_path']} to #{self.temporary_directory}"
-      FileUtils.cp_r(self.config['template_path'], self.temporary_directory)
+      self.files['pass.json'] = JSON.pretty_generate(self.json)
     end
 
     def write_translation_strings
       self.translations.each do |language, trans|
-        translations_path = "#{self.temporary_path}/#{language}.lproj"
-        FileUtils.mkdir_p(translations_path) unless File.directory?(translations_path)
-        File.open("#{translations_path}/pass.strings", "a") do |f|
-          trans.each do |key, value|
-            f.puts "\"#{key}\" = \"#{value}\";"
-          end
+        self.files["#{language}.lproj/pass.strings"] ||= ""
+        trans.each do |key, value|
+          self.files["#{language}.lproj/pass.strings"] << "\"#{key}\" = \"#{value}\";\n"
         end
-      end
-    end
-
-    # Creates a temporary place to work with the pass files without polluting the original
-    def create_temporary_directory
-      self.temporary_directory = Dir.mktmpdir
-      puts "Creating temp dir at #{self.temporary_directory}"
-      self.temporary_path = self.temporary_directory + "/" + self.pass_type_id
-      # Check if the directory exists
-      if File.directory?(self.temporary_path)
-        # Need to clean up the directory
-        FileUtils.rm_rf(self.temporary_path)
-      end
-      FileUtils.mkdir_p(self.temporary_path)
-    end
-
-    def clean_ds_store_files
-      puts "Cleaning .DS_Store files"
-      Dir.glob(self.temporary_path + "**/.DS_Store").each do |file|
-        File.delete(file)
       end
     end
 
@@ -108,52 +56,33 @@ module Passbook
     def generate_json_manifest
       puts "Generating JSON manifest"
       manifest = {}
-
       # Gather all the files and generate a sha1 hash
-      Dir.glob(self.temporary_path + "/**/**").each do |file|
-        manifest[file[self.temporary_path.length+1..-1].gsub("/", "\/")] = Digest::SHA1.hexdigest(File.read(file)) unless File.directory?(file)
+      self.files.each do |filename, content|
+        manifest[filename] = Digest::SHA1.hexdigest(content)
       end
 
-      # Write the hash dictionary out to a manifest file
-      self.manifest_url = self.temporary_path + "/manifest.json"
-      File.open(self.manifest_url, "w") do |f|
-        f.write(JSON.pretty_generate(manifest))
-      end
+      self.files['manifest.json'] = JSON.pretty_generate(manifest)
     end
 
     def sign_manifest
       puts "Signing the manifest"
-      # Import the certificate
-      p12_certificate = OpenSSL::PKCS12::new(File.read(config['cert_path']), config['cert_password'])
-      wwdr_certificate = OpenSSL::X509::Certificate.new(File.read(Passbook.wwdr_intermediate_certificate_path))
 
-      # Sign the data
       flag = OpenSSL::PKCS7::BINARY|OpenSSL::PKCS7::DETACHED
-      signed = OpenSSL::PKCS7::sign(p12_certificate.certificate, p12_certificate.key, File.read(self.manifest_url), [wwdr_certificate], flag)
+      signed = OpenSSL::PKCS7::sign(config['p12_certificate'].certificate, config['p12_certificate'].key, self.files['manifest.json'], [Passbook.wwdr_certificate], flag)
 
-      # Create an output path for the signed data
-      signature_url = self.temporary_path + "/signature"
-
-      # Write out the data
-      File.open(signature_url, "w") do |f|
-        f.write signed.to_der.force_encoding('UTF-8')
-      end
+      self.files['signature'] = signed.to_der.force_encoding('UTF-8')
     end
 
     def compress_pass_file
       puts "Compressing the pass"
-      zipped_file = File.open(self.output_url, "w")
-
-      #Matching pattern was "/**" and now changed to ".*"
-      Zip::ZipOutputStream.open(zipped_file.path) do |z|
-        Dir.glob(self.temporary_path + "/**/**").each do |file|
-          unless File.directory?(file)
-            z.put_next_entry(file[self.temporary_path.length+1..-1])
-            z.print IO.read(file)
-          end
+      stringio = Zip::ZipOutputStream::write_buffer do |z|
+        self.files.each do |filename, content|
+          z.put_next_entry filename
+          z.print content
         end
       end
-      zipped_file
+      stringio.rewind
+      stringio.sysread
     end
   end
 end
