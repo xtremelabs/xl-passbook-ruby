bb1b2e83b131544af8755b2f390ce70d73c77c03
Andrei Dinin <andrei.dinin@xtremelabs.com>
2012-10-21 16:34:00 -0400

    #2 - adding a :pkpass renderer. use it as 'render :pkpass=>model_instance, :pass_type_id=>"pass.com.acme"'  where your model implements :serial_number :update_pass pkpass

--- lib/passbook-ruby.rb
+++ lib/passbook-ruby.rb
@@ -1,6 +1,17 @@
 require "passbook/config"
 require "passbook/pkpass"
 
+require "action_controller"
+Mime::Type.register 'application/vnd.apple.pkpass', :pkpass
+
+ActionController::Renderers.add :pkpass do |obj, options|
+  pkpass = Passbook::Pkpass.new options[:pass_type_id], obj.serial_number
+  obj.update_pass pkpass
+  pkpass_io = pkpass.package
+  response.headers["last-modified"] = obj.updated_at.strftime("%Y-%m-%d %H:%M:%S")
+  send_data(pkpass_io.sysread, :type => 'application/vnd.apple.pkpass', :disposition=>'inline', :filename=>"#{obj.serial_number}.pkpass")
+end
+
 module Passbook
 end
 
