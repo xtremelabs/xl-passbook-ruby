f2260b828cb7fc955488715f1755084706f06f04
Andrei D & Vincent L <andrei.dinin@xtremelabs.com>
2012-10-25 19:01:16 -0400

    minor typo

--- lib/rails/generators/passbook/pkpass/templates/initializer.rb
+++ lib/rails/generators/passbook/pkpass/templates/initializer.rb
@@ -2,7 +2,7 @@ Passbook::Config.instance.add_pkpass do |passbook|
   passbook.pass_config["<%= pass_type_id %>"]={
                               "cert_path"=>"<%= cert_path %>",
                               "cert_password"=>"<%= cert_password %>",
-                              "template_path"=>"<%= template_path %>"
+                              "template_path"=>"<%= template_path %>",
                               "class"=>"<%= class_name %>"
                             }
 
