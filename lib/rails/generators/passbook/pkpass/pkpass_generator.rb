7586eba3870a38afeddd5ed60f31325c707d22c2
Andrei Dinin <andrei.dinin@xtremelabs.com>
2012-10-26 10:44:13 -0400

    improving defaults for pkpass generator

--- lib/rails/generators/passbook/pkpass/pkpass_generator.rb
+++ lib/rails/generators/passbook/pkpass/pkpass_generator.rb
@@ -3,12 +3,14 @@ module Passbook
     class PkpassGenerator < Rails::Generators::NamedBase
       source_root File.expand_path('../templates', __FILE__)
       argument :pass_type_id, type: :string, default: 'pass.com.acme', optional: true, banner: "The Pass Type ID you registered at iOS Provisioning Portal"
-      argument :template_path, type: :string, default: '#{Rails.root}/data/templates/pass.com.acme', optional: true, banner: "Absolute path to the pass template"
-      argument :cert_path, type: :string, default: '#{Rails.root}/data/certificates/pass.com.acme.p12', optional: true, banner: "Absolute path to your pass.com.acme.p12 file"
+      argument :template_path, type: :string, optional: true, banner: "Absolute path to the pass template"
+      argument :cert_path, type: :string, optional: true, banner: "Absolute path to your pass.com.acme.p12 file"
       argument :cert_password, type: :string, default: 'password', optional: true, banner: "Password for your P12 certificate"
 
       desc 'Create and configure a model for a particular pkpass (ie. ticket)'
       def create_initializer_file
+        template_path ||= "#{Rails.root}/data/templates/#{pass_type_id}"
+        cert_path ||= "#{Rails.root}/data/certificates#{pass_type_id}"
         template 'initializer.rb', File.join('config', 'initializers', "passbook_#{plural_name.singularize}.rb")
         template 'model.rb', File.join('app', 'models', "#{plural_name.singularize}.rb")
         route "get 'passes/#{plural_name.singularize}' => 'Passbook::passes#get_pkpass', :defaults => { :pass_type_id => '#{pass_type_id}' }"
